31d4c5c72c7548565825ea7804de9129b2972ef0
rajibsutradhartonmoy <rajibsutradhartonmoy@gmail.com>
2023-12-19 06:55:30 +0600

    updated regions

--- src/assets/data/regions.js
+++ src/assets/data/regions.js
@@ -1,12 +1,7 @@
 export const regions = [
-  { name: 'North America', code: 'NA' },
-  { name: 'Europe', code: 'EU' },
-  { name: 'Asia', code: 'AS' },
-  { name: 'South America', code: 'SA' },
-  { name: 'Africa', code: 'AF' },
-  { name: 'Oceania', code: 'OC' },
-  { name: 'Antarctica', code: 'AN' },
-  { name: 'Middle East', code: 'ME' },
-  { name: 'Central America', code: 'CA' },
-  { name: 'Caribbean', code: 'CRB' },
+  { name: 'Northeast', code: 'Northeast' },
+  { name: 'Southwest', code: 'Southwest' },
+  { name: 'West', code: 'West' },
+  { name: 'Southeast', code: 'Southeast' },
+  { name: 'Midwest', code: 'Midwest' },
 ];
