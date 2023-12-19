545bd42577ffbfb8247ffb653e700d4c8496e06a
rajibsutradhartonmoy <rajibsutradhartonmoy@gmail.com>
2023-12-19 07:14:40 +0600

    added cancel button in warehoue create and edit page

--- src/sections/warehouse/warehouse-create/warehouse-create-fields.js
+++ src/sections/warehouse/warehouse-create/warehouse-create-fields.js
@@ -1,9 +1,11 @@
 // import PropTypes from 'prop-types'
 
-import { Button, Grid } from '@mui/material';
+import { Button, Grid, Stack } from '@mui/material';
+import Link from 'next/link';
 import { RHFTextField } from 'src/components/hook-form';
 import Label from 'src/components/label';
 import { WarehousePhotoUpload } from 'src/components/warehouse/upload';
+import { paths } from 'src/routes/paths';
 
 const WarehouseCreateFields = (props) => (
   <Grid container maxWidth={600} spacing={1.2}>
@@ -31,9 +33,29 @@ const WarehouseCreateFields = (props) => (
     </Grid>
 
     <Grid item xs={12} textAlign="right" mt={5}>
-      <Button variant="contained" size="large" type="submit" color="primary">
-        Save Changes
-      </Button>
+      <Stack
+        sx={{
+          flexDirection: {
+            xs: 'row',
+            sm: 'row-reverse',
+          },
+          justifyContent: {
+            xs: 'start',
+            sm: 'end',
+          },
+        }}
+        flexWrap="wrap"
+        spacing={0.5}
+      >
+        <Button variant="contained" size="large" type="submit" color="primary">
+          Create Warehouse
+        </Button>
+        <Link href={paths.dashboard.listing}>
+          <Button variant="soft" size="large" color="error" type="reset">
+            Cancel
+          </Button>
+        </Link>
+      </Stack>
     </Grid>
   </Grid>
 );
