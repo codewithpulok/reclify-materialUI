5c4272034a33c6b8c0f7f283a1ffff9002231526
rajibsutradhartonmoy <rajibsutradhartonmoy@gmail.com>
2023-12-20 17:07:34 +0600

    initial map postion updated

--- src/components/common/map/presets/map-marked.js
+++ src/components/common/map/presets/map-marked.js
@@ -1,6 +1,6 @@
 import 'mapbox-gl/dist/mapbox-gl.css';
 import PropTypes from 'prop-types';
-import { memo } from 'react';
+import { memo, useMemo } from 'react';
 import Map from 'react-map-gl';
 
 import { MapControl, MapMarker } from 'src/components/common/map';
@@ -19,19 +19,16 @@ const initialViewState = { zoom: 2 };
  */
 const MapMarked = (props) => {
   const { markedData, ...other } = props;
+  const initialPosition = useMemo(
+    () => ({ longitude: markedData?.[0]?.latlng?.[1], latitude: markedData?.[0]?.latlng?.[0] }),
+    [markedData]
+  );
 
   return (
-    <Map initialViewState={initialViewState} {...other}>
+    <Map initialViewState={{ ...initialViewState, ...initialPosition }} {...other}>
       <MapControl />
       {markedData.map((city, index) => (
-        <MapMarker
-          key={`marker-${index}`}
-          latitude={city.latlng[0]}
-          longitude={city.latlng[1]}
-          // onClick={(event) => {
-          //   event.originalEvent.stopPropagation();
-          // }}
-        />
+        <MapMarker key={`marker-${index}`} latitude={city.latlng[0]} longitude={city.latlng[1]} />
       ))}
     </Map>
   );
