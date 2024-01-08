f27123d6db6aa3a6ec0f1bf862f0f47c4e17b8b4
rajibsutradhartonmoy <rajibsutradhartonmoy@gmail.com>
2024-01-08 18:22:59 +0600

    added pessimistic update to warehouse api

--- src/redux-toolkit/services/warehouseApi.js
+++ src/redux-toolkit/services/warehouseApi.js
@@ -17,6 +17,21 @@ export const warehouseApi = createApi({
         body: warehouseData,
         method: 'POST',
       }),
+      onQueryStarted: async (warehouse, { dispatch, queryFulfilled }) => {
+        try {
+          const response = await queryFulfilled;
+          const { data } = response;
+
+          // update the waehouse list cache
+          dispatch(
+            warehouseApi.util.updateQueryData('warehouseList', undefined, (draft) => {
+              draft.results.push(data.results);
+            })
+          );
+        } catch (error) {
+          // console.log("Warehouse Create API Error: ", error);
+        }
+      },
     }),
     warehouseUpdate: builder.mutation({
       query: ({ id, warehouseData }) => ({
@@ -24,12 +39,49 @@ export const warehouseApi = createApi({
         body: warehouseData,
         method: 'PUT',
       }),
+      onQueryStarted: async ({ id, warehouse }, { dispatch, queryFulfilled }) => {
+        try {
+          const response = await queryFulfilled;
+          const { data } = response;
+
+          // update the waehouse list cache
+          dispatch(
+            warehouseApi.util.updateQueryData('warehouseList', undefined, (draft) => {
+              const updateIndex = draft.results.findIndex((w) => w.id === id);
+              if (updateIndex !== -1) draft.results[updateIndex] = data?.results;
+            })
+          );
+          // update the individual warehouse cache
+          dispatch(
+            warehouseApi.util.updateQueryData('warehouse', id, (draft) => {
+              draft.results = data.results;
+            })
+          );
+        } catch (error) {
+          // console.log("Warehouse Update API Error: ", error);
+        }
+      },
     }),
     warehouseDelete: builder.mutation({
       query: (id) => ({
         url: `/${id}`,
         method: 'DELETE',
       }),
+      onQueryStarted: async (id, { dispatch, queryFulfilled }) => {
+        try {
+          await queryFulfilled;
+
+          // update the waehouse list cache
+          dispatch(
+            warehouseApi.util.updateQueryData('warehouseList', undefined, (draft) => {
+              const filtered = draft.results.filter((w) => w.id !== id);
+              draft = filtered;
+            })
+          );
+        } catch (error) {
+          // console.log("Warehouse Update API Error: ", error);
+        }
+      },
     }),
   }),
 });
