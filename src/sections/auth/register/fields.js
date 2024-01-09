676d80f3d720a4aa1ec5281af517b61ad25260c0
rajibsutradhartonmoy <rajibsutradhartonmoy@gmail.com>
2024-01-09 11:03:38 +0600

    removed region from signup page

--- src/sections/auth/register/fields.js
+++ src/sections/auth/register/fields.js
@@ -1,7 +1,6 @@
 import Stack from '@mui/material/Stack';
 
 import { MenuItem } from '@mui/material';
-import { regions } from 'src/assets/data';
 import { PasswordField } from 'src/components/common/fields';
 import { RHFTextField } from 'src/components/common/hook-form';
 
@@ -12,17 +11,9 @@ const Fields = (props) => (
       <RHFTextField name="lastName" label="Last name" />
     </Stack>
 
-    <RHFTextField name="region" label="Region" select>
-      {regions.map((option) => (
-        <MenuItem key={option.code} value={option.code}>
-          {option.name}
-        </MenuItem>
-      ))}
-    </RHFTextField>
-
     <RHFTextField name="userType" label="Account Type" select>
       <MenuItem value="customer">Customer</MenuItem>
-      <MenuItem value="seller">Seller</MenuItem>
+      <MenuItem value="seller">Service Provider</MenuItem>
     </RHFTextField>
 
     <RHFTextField name="email" label="Email address" />
