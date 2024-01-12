eb0f4d1da25e68f6949ecb687c12124000a3e203
rajibsutradhartonmoy <rajibsutradhartonmoy@gmail.com>
2024-01-12 20:14:32 +0600

    updated owner card

--- src/components/warehouse/cards/warehouse-owner-card.js
+++ src/components/warehouse/cards/warehouse-owner-card.js
@@ -1,9 +1,7 @@
-import { Box, Button, Card, CardActionArea, Stack, Typography } from '@mui/material';
+import { Box, Button, Card, Stack, Typography } from '@mui/material';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
 // local components
-import { useRouter } from 'next/navigation';
-import { useCallback } from 'react';
 import Image from 'src/components/common/image';
 import Label from 'src/components/common/label';
 import { RouterLink } from 'src/routes/components';
@@ -24,100 +22,104 @@ const Props = {
  */
 const WarehouseOwnerCard = (props) => {
   const { user, sx, clickable = false } = props;
-  const router = useRouter();
-
-  const gotoSellerProfile = useCallback(() => {
-    router.push(`${paths.dashboard.users.sellers}/${user.id}`);
-  }, [user, router]);
 
   return (
-    <Card sx={sx}>
-      <CardActionArea
-        onClick={clickable ? gotoSellerProfile : undefined}
+    <Card
+      sx={{
+        p: { xs: 1.5, sm: 2 },
+        borderRadius: 1,
+        '& > .MuiCardActionArea-focusHighlight': {
+          background: 'transparent',
+        },
+        ...sx,
+      }}
+    >
+      <Stack
         sx={{
-          p: { xs: 1.5, sm: 2 },
-          borderRadius: 1,
-          '& > .MuiCardActionArea-focusHighlight': {
-            background: 'transparent',
+          flexDirection: {
+            xs: 'column',
+            sm: 'row',
           },
+          alignItems: 'start',
         }}
+        direction="row"
+        alignItems="center"
+        spacing={1.5}
       >
-        <Stack
+        <Box
           sx={{
-            flexDirection: {
-              xs: 'column',
-              sm: 'row',
-            },
-            alignItems: 'start',
+            width: '90px',
           }}
-          direction="row"
-          alignItems="center"
-          spacing={1.5}
         >
-          <Box
-            sx={{
-              width: '90px',
-            }}
-          >
-            <Image
-              alt={user.displayName}
-              src={user.avatar}
-              ratio="1/1"
-              sx={{ borderRadius: 100 }}
-            />
-          </Box>
-          <Box sx={{ width: '100%', flex: 1 }}>
-            <Stack direction="row" alignItems="center" justifyContent="space-between" mb={0.8}>
-              <Typography variant="h6">{user.displayName}</Typography>
+          <Image alt={user.displayName} src={user.avatar} ratio="1/1" sx={{ borderRadius: 100 }} />
+        </Box>
+        <Box sx={{ width: '100%', flex: 1 }}>
+          <Stack direction="row" alignItems="center" spacing={1} mb={0.8}>
+            <Typography variant="h6">{user.displayName}</Typography>
 
-              <Label color="primary" size="small" variant="soft">
-                owner
-              </Label>
-            </Stack>
-            <Stack direction="row" alignItems="center" spacing={0.5}>
-              {ICONS.email(18)}
+            <Label color="primary" size="small" variant="soft">
+              owner
+            </Label>
 
-              <Typography
-                variant="body2"
-                color="text.primary"
-                sx={{ textDecoration: 'none' }}
-                component={Link}
-                href={`mailto:${user.email}`}
-                onClick={(e) => e.stopPropagation()}
-              >
-                {user.email}
-              </Typography>
-            </Stack>
-            <Stack direction="row" alignItems="center" spacing={0.5}>
-              {ICONS.phone(18)}
+            <Button
+              LinkComponent={RouterLink}
+              href={`${paths.dashboard.messages.root}?user=${user.id}`}
+              color="primary"
+              variant="outlined"
+              startIcon={ICONS.send_message()}
+              size="small"
+              onClick={(e) => e.stopPropagation()}
+              sx={{ ml: 'auto' }}
+            >
+              Send a message
+            </Button>
+          </Stack>
+          <Stack direction="row" alignItems="center" spacing={0.5}>
+            {ICONS.email(18)}
 
-              <Typography
-                variant="body2"
-                color="text.primary"
-                sx={{ textDecoration: 'none' }}
-                component={Link}
-                href={`tel:${user.phoneNumber}`}
-                onClick={(e) => e.stopPropagation()}
-              >
-                {user.phoneNumber}
-              </Typography>
-            </Stack>
+            <Typography
+              variant="body2"
+              color="text.primary"
+              sx={{ textDecoration: 'none' }}
+              component={Link}
+              href={`mailto:${user.email}`}
+              onClick={(e) => e.stopPropagation()}
+            >
+              {user.email}
+            </Typography>
+          </Stack>
+          <Stack direction="row" alignItems="center" spacing={0.5}>
+            {ICONS.phone(18)}
+
+            <Typography
+              variant="body2"
+              color="text.primary"
+              sx={{ textDecoration: 'none' }}
+              component={Link}
+              href={`tel:${user.phoneNumber}`}
+              onClick={(e) => e.stopPropagation()}
+            >
+              {user.phoneNumber}
+            </Typography>
+          </Stack>
+          {clickable && (
             <Stack direction="row" alignItems="center" spacing={0.5} mt={2} justifyContent="end">
               <Button
                 LinkComponent={RouterLink}
-                href={`${paths.dashboard.messages.root}?user=${user.id}`}
-                color="primary"
+                href={`${paths.dashboard.users.sellers}/${user.id}`}
+                color="secondary"
                 variant="outlined"
                 startIcon={ICONS.send_message()}
                 size="small"
                 onClick={(e) => e.stopPropagation()}
+                sx={{ ml: 'auto' }}
               >
-                Send a message
+                Visit profile
               </Button>
             </Stack>
-          </Box>
-        </Stack>
-      </CardActionArea>
+          )}
+        </Box>
+      </Stack>
     </Card>
   );
 };
