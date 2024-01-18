25274f9a02856803c96587ad55902692e0712062
rajibsutradhartonmoy <rajibsutradhartonmoy@gmail.com>
2024-01-18 19:53:12 +0600

    worked on owner card

--- src/components/warehouse/cards/warehouse-owner-card.js
+++ src/components/warehouse/cards/warehouse-owner-card.js
@@ -1,5 +1,4 @@
-import { Box, Button, Card, Stack, Typography } from '@mui/material';
-import Link from 'next/link';
+import { Box, Card, Chip, Link, Stack, Typography } from '@mui/material';
 import PropTypes from 'prop-types';
 // local components
 import Image from 'src/components/common/image';
@@ -52,13 +51,27 @@ const WarehouseOwnerCard = (props) => {
         >
           <Image alt={user.displayName} src={user.avatar} ratio="1/1" sx={{ borderRadius: 100 }} />
         </Box>
-        <Box sx={{ width: '100%', flex: 1 }}>
-          <Stack direction="row" alignItems="center" spacing={1} mb={0.8}>
+        <Stack spacing={0.2} sx={{ width: '100%', flex: 1 }}>
+          <Stack direction="row" alignItems="center" flexWrap="wrap" spacing={1} mb={0.8}>
             <Typography variant="h6">{user.displayName}</Typography>
 
-            <Label color="primary" size="small" variant="soft">
+            <Label color="primary" size="small" variant="soft" sx={{ mr: 'auto' }}>
               seller
             </Label>
+
+            <Link
+              component={RouterLink}
+              sx={{ width: { xs: '100%', sm: 'auto' } }}
+              href={`${paths.dashboard.messages.root}?id=${user.id}`}
+            >
+              <Chip
+                sx={{ width: { xs: '100%', sm: 'auto' } }}
+                icon={ICONS.send_message()}
+                label="Send Message"
+                variant="outlined"
+                clickable
+              />
+            </Link>
           </Stack>
           <Stack direction="row" alignItems="center" spacing={0.5}>
             {ICONS.email(18)}
@@ -67,54 +80,43 @@ const WarehouseOwnerCard = (props) => {
               variant="body2"
               color="text.primary"
               sx={{ textDecoration: 'none' }}
-              component={Link}
+              component={RouterLink}
               href={`mailto:${user.email}`}
               onClick={(e) => e.stopPropagation()}
             >
               {user.email}
             </Typography>
           </Stack>
-          <Stack direction="row" alignItems="center" spacing={0.5}>
+          <Stack direction="row" alignItems="center" flexWrap="wrap" spacing={0.5}>
             {ICONS.phone(18)}
 
             <Typography
               variant="body2"
               color="text.primary"
-              sx={{ textDecoration: 'none' }}
-              component={Link}
+              sx={{ textDecoration: 'none', mr: 'auto' }}
+              component={RouterLink}
               href={`tel:${user.phoneNumber}`}
               onClick={(e) => e.stopPropagation()}
             >
               {user.phoneNumber}
             </Typography>
-          </Stack>
-          <Stack direction="row" alignItems="center" spacing={0.5} mt={2} justifyContent="start">
-            <Button
-              LinkComponent={RouterLink}
-              href={`${paths.dashboard.messages.root}?user=${user.id}`}
-              color="primary"
-              variant="contained"
-              startIcon={ICONS.send_message()}
-              size="small"
-              onClick={(e) => e.stopPropagation()}
-            >
-              Send a message
-            </Button>
+
             {clickable && (
-              <Button
-                LinkComponent={RouterLink}
+              <Link
+                component={RouterLink}
+                sx={{ width: { xs: '100%', sm: 'auto' } }}
                 href={`${paths.dashboard.users.sellers}/${user.id}`}
-                color="secondary"
-                variant="contained"
-                startIcon={ICONS.send_message()}
-                size="small"
-                onClick={(e) => e.stopPropagation()}
               >
-                Visit profile
-              </Button>
+                <Chip
+                  sx={{ width: { xs: '100%', sm: 'auto' } }}
+                  label=" Visit profile"
+                  color="primary"
+                  clickable
+                />
+              </Link>
             )}
           </Stack>
-        </Box>
+        </Stack>
       </Stack>
     </Card>
   );
