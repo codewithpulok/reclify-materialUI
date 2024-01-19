192e9824ba745f4631054b599cc329e255c846ec
dev <dan.devguru@gmail.com>
2024-01-19 10:00:20 -0500

    Fix next build issue

--- src/sections/contact/contact-form.js
+++ src/sections/contact/contact-form.js
@@ -14,14 +14,14 @@ export default function ContactForm() {
     <Stack component={MotionViewport} spacing={5}>
       <Stack spacing={1}>
         <Typography>
-          Whether you're taking the first steps toward listing your services or embarking on a
+          {`Whether you're taking the first steps toward listing your services or embarking on a
           search for the ideal warehousing space, Racklify is your partner in navigating the
-          logistics landscape.{' '}
+          logistics landscape.`}
         </Typography>
         <Typography>
-          We welcome your inquiries and are eager to assist you here at Racklify. Whether you're a
+          {`We welcome your inquiries and are eager to assist you here at Racklify. Whether you're a
           warehouse or service provider seeking to join our esteemed platform or an individual in
-          search of warehouse space, our team is ready to provide the support you need.{' '}
+          search of warehouse space, our team is ready to provide the support you need.`}
         </Typography>
         <Typography>
           Feel free to reach out if you have questions, require guidance, or simply want to explore
