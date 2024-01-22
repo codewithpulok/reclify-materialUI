d1cbebb832c21b33c0d5dc90649646b7b31a1775
rajibsutradhartonmoy <rajibsutradhartonmoy@gmail.com>
2024-01-22 16:57:50 +0600

    added socials tab

--- src/types/user.types.js
+++ src/types/user.types.js
@@ -10,21 +10,16 @@
  * @property {string} password - The password of the user (Note: This should be stored securely and not exposed).
  * @property {string} avatar - The URL of the user's profile photo.
  * @property {string} phoneNumber - The phone number of the user.
- * @property {string} country - The country of residence for the user.
- * @property {Address} address - The street address of the user.
- * @property {string} state - The state or province of the user.
- * @property {string} city - The city of residence for the user.
- * @property {string} zipCode - The ZIP or postal code of the user's address.
- * @property {string} about - A brief description or bio of the user.
  * @property {UserType} userType - The type of the user ('admin', 'customer', 'seller').
- * @property {string} region - The region of residence for the user.
+ * @property {Address} address - The street address of the user.
+ * @property {string} about - A brief description or bio of the user.
  * @property {number} createdAt - The timestamp when the user account was created.
  * @property {number} updatedAt - The timestamp when the user account was last updated.
- * @property {Object} social - The social media links of the user.
- * @property {string} social.facebook - The Facebook profile link.
- * @property {string} social.instagram - The Instagram profile link.
- * @property {string} social.linkedin - The LinkedIn profile link.
- * @property {string} social.x - Additional social link.
+ * @property {Object} socials - The social media links of the user.
+ * @property {string} socials.facebook - The Facebook profile link.
+ * @property {string} socials.instagram - The Instagram profile link.
+ * @property {string} socials.linkedin - The LinkedIn profile link.
+ * @property {string} socials.x - Additional social link.
  */
 
 /**
