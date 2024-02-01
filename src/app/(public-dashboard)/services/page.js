c0c2bc95155db5000b25e0f7fd1d65b013f652fb
rajibsutradhartonmoy <rajibsutradhartonmoy@gmail.com>
2024-02-01 21:05:50 +0600

    worked on public route

---
+++ src/app/(public-dashboard)/services/page.js
@@ -0,0 +1,11 @@
+import { ServicesListingView } from 'src/sections/services';
+
+export const metadata = {
+  title: 'Services - Racklify',
+};
+
+const ServicesListingPage = (props) => <ServicesListingView />;
+
+ServicesListingPage.propTypes = {};
+
+export default ServicesListingPage;
