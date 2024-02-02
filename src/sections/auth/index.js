3da903f261a101607014ec0cf1df2344c75fc5ba
rajibsutradhartonmoy <rajibsutradhartonmoy@gmail.com>
2024-02-02 21:22:41 +0600

    added auth action files

--- src/sections/auth/index.js
+++ src/sections/auth/index.js
@@ -1,2 +1,7 @@
 export { default as LoginView } from './login/view';
 export { default as RegisterView } from './register/view';
+
+// auth action page
+export { default as EmailVerifyView } from './email-verify/view';
+export { default as ForgotPasswordView } from './forgot-password/view';
+export { default as NewPasswordView } from './new-password/view';
---
+++ src/sections/auth/new-password/view.js
@@ -0,0 +1,145 @@
+'use client';
+
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+import * as Yup from 'yup';
+
+import LoadingButton from '@mui/lab/LoadingButton';
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
+import Link from '@mui/material/Link';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+
+import { RouterLink } from 'src/routes/components';
+import { paths } from 'src/routes/paths';
+
+import { useBoolean } from 'src/hooks/use-boolean';
+
+import { SentIcon } from 'src/assets/icons';
+
+import FormProvider, { RHFTextField } from 'src/components/common/hook-form';
+import Iconify from 'src/components/common/iconify';
+
+// ----------------------------------------------------------------------
+
+export default function NewPasswordView() {
+  const password = useBoolean();
+
+  const NewPasswordSchema = Yup.object().shape({
+    code: Yup.string().min(6, 'Code must be at least 6 characters').required('Code is required'),
+    email: Yup.string().required('Email is required').email('Email must be a valid email address'),
+    password: Yup.string()
+      .min(6, 'Password must be at least 6 characters')
+      .required('Password is required'),
+    confirmPassword: Yup.string()
+      .required('Confirm password is required')
+      .oneOf([Yup.ref('password')], 'Passwords must match'),
+  });
+
+  const defaultValues = {
+    token: '',
+    password: '',
+    confirmPassword: '',
+  };
+
+  const methods = useForm({
+    mode: 'onChange',
+    resolver: yupResolver(NewPasswordSchema),
+    defaultValues,
+  });
+
+  const {
+    handleSubmit,
+    formState: { isSubmitting },
+  } = methods;
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 500));
+      console.info('DATA', data);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  const renderForm = (
+    <Stack spacing={3} alignItems="center">
+      <RHFTextField
+        name="password"
+        label="Password"
+        type={password.value ? 'text' : 'password'}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton onClick={password.onToggle} edge="end">
+                <Iconify icon={password.value ? 'solar:eye-bold' : 'solar:eye-closed-bold'} />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+
+      <RHFTextField
+        name="confirmPassword"
+        label="Confirm New Password"
+        type={password.value ? 'text' : 'password'}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton onClick={password.onToggle} edge="end">
+                <Iconify icon={password.value ? 'solar:eye-bold' : 'solar:eye-closed-bold'} />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+
+      <LoadingButton
+        fullWidth
+        size="large"
+        type="submit"
+        variant="contained"
+        loading={isSubmitting}
+      >
+        Update Password
+      </LoadingButton>
+
+      <Link
+        component={RouterLink}
+        href={paths.auth.login}
+        color="inherit"
+        variant="subtitle2"
+        sx={{
+          alignItems: 'center',
+          display: 'inline-flex',
+        }}
+      >
+        <Iconify icon="eva:arrow-ios-back-fill" width={16} />
+        Return to sign in
+      </Link>
+    </Stack>
+  );
+
+  const renderHead = (
+    <>
+      <SentIcon sx={{ height: 96 }} />
+
+      <Stack spacing={1} sx={{ my: 5 }}>
+        <Typography variant="h3">Change Your Password!</Typography>
+
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, at.
+        </Typography>
+      </Stack>
+    </>
+  );
+
+  return (
+    <FormProvider methods={methods} onSubmit={onSubmit}>
+      {renderHead}
+
+      {renderForm}
+    </FormProvider>
+  );
+}
