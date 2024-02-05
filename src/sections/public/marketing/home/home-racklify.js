c48404ba6948bb17dcbd89fe33a127b0f4007746
rajibsutradhartonmoy <rajibsutradhartonmoy@gmail.com>
2024-02-05 16:47:43 +0600

    updated images

--- src/sections/public/marketing/home/home-racklify.js
+++ src/sections/public/marketing/home/home-racklify.js
@@ -9,44 +9,44 @@ import Typography from '@mui/material/Typography';
 
 import { Grid } from '@mui/material';
 import { MotionViewport, varFade } from 'src/components/common/animate';
-import { getIconify } from 'src/components/common/iconify/utilities';
+import Image from 'src/components/common/image';
 
 // ----------------------------------------------------------------------
 
 const CARDS = [
   {
     id: '1',
-    icon: 'noto:rocket',
+    image: 'mini_rocket.png',
     title: 'Welcome to the future of warehousing!',
     description: `At Racklify, we're reshaping the logistics industry by introducing a groundbreaking platform that connects businesses with top warehousing and fulfillment providers across the country. Say goodbye to uncertainty and unexpected costs - Racklify guarantees the best rates and ensures a seamless booking experience for both warehouse providers and users.`,
   },
   {
     id: '2',
-    icon: 'noto:globe-with-meridians',
+    image: 'mini_earth.png',
     title: 'Discover Your Perfect Space',
     description: `Tired of sifting through endless options? With Racklify, finding the ideal storage space is a breeze. Our platform showcases a curated selection of vetted warehouses, offering on-demand storage solutions at guaranteed prices. Whether you need short-term or long-term storage, we've got you covered.`,
   },
   {
     id: '3',
-    icon: 'noto:briefcase',
+    image: 'mini_suitcase.png',
     title: 'Save Big with Guaranteed Rates',
     description: `No more hidden fees or unexpected charges. Racklify eliminates the guesswork by providing transparent and guaranteed pallet rates. We've given the tools to warehouses to offer the best rates, saving you money and ensuring a stress-free storage experience.`,
   },
   {
     id: '4',
-    icon: 'noto:alarm-clock',
+    image: 'mini_alarm.png',
     title: 'HotRacks - Limited Time Deals',
     description: `Ready for unbeatable savings? Explore our exclusive HotRacks, where deeply discounted and time-limited listings await. Jump on these promotions to enjoy even more cost-effective storage solutions. It's a win-win for businesses seeking budget-friendly options as well as warehouses with extra capacity.`,
   },
   {
     id: '5',
-    icon: 'noto:counterclockwise-arrows-button',
+    image: 'mini_owner.png',
     title: 'Matchmaking Made Easy',
     description: `Are you a warehouse owner looking to maximize your space utilization? Or a business in need of reliable storage? Racklify is the perfect matchmaker! Join our two-sided marketplace and unlock a world of opportunities for both warehouse and end user`,
   },
   {
     id: '6',
-    icon: 'noto:package',
+    image: 'mini_box.png',
     title: 'Beyond Storage',
     description: `Racklify isn't just about storage - it's a comprehensive platform offering various services to streamline your logistics. From freight and consulting to packaging and manufacturing, we've got everything you need under one roof.`,
   },
@@ -93,7 +93,7 @@ export default function HomeRacklify() {
                 p: (theme) => theme.spacing(10, 5),
               }}
             >
-              {getIconify(card.icon, 58)}
+              <Image src={card.image} sx={{ width: '100px' }} />
 
               <Typography variant="h5" sx={{ mt: 7, mb: 2 }}>
                 {card.title}
