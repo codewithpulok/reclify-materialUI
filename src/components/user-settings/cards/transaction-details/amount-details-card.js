9236cc06b1a081908f944a9b7196aa8f4f968761
rajibsutradhartonmoy <rajibsutradhartonmoy@gmail.com>
2024-02-15 21:01:20 +0600

    updated amount details for transaction

--- src/components/user-settings/cards/transaction-details/amount-details-card.js
+++ src/components/user-settings/cards/transaction-details/amount-details-card.js
@@ -1,4 +1,4 @@
-import { Card } from '@mui/material';
+import { Card, Divider } from '@mui/material';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import PropTypes from 'prop-types';
@@ -21,39 +21,38 @@ const AmountDetailsCard = (props) => {
   return (
     <Card component={Stack} sx={{ p: 1.5, borderRadius: 1, ...sx }} spacing={0.5}>
       <Stack direction="row" justifyContent="space-between" alignItems="baseline" spacing={1}>
-        <Typography variant="subtitle2">Total Pallet:</Typography>
+        <Typography variant="subtitle2">Selected Term (months): </Typography>
         <Typography variant="subtitle2" color="text.secondary">
-          {fNumber(purchase?.pallet)}
+          {purchase?.selectedTerm || '-'}
         </Typography>
       </Stack>
       <Stack direction="row" justifyContent="space-between" alignItems="baseline" spacing={1}>
         <Typography variant="subtitle2">Price Per Pallet: </Typography>
         <Typography variant="subtitle2" color="text.secondary">
-          {fCurrency(purchase?.selectedPrice) || '-'}
+          {fCurrency(purchase?.pricePerPallet)}
+        </Typography>
+      </Stack>
+      {!!purchase?.discountedPricePerPallet && (
+        <Stack direction="row" justifyContent="space-between" alignItems="baseline" spacing={1}>
+          <Typography variant="subtitle2">Discounted Price Per Pallet: </Typography>
+          <Typography variant="subtitle2" color="text.secondary">
+            {fCurrency(purchase?.discountedPricePerPallet)}
+          </Typography>
+        </Stack>
+      )}
+      <Stack direction="row" justifyContent="space-between" alignItems="baseline" spacing={1}>
+        <Typography variant="subtitle2">Quantity of Pallet: </Typography>
+        <Typography variant="subtitle2" color="text.secondary">
+          {fNumber(purchase?.quantityOfPallet)}
         </Typography>
       </Stack>
       <Stack direction="row" justifyContent="space-between" alignItems="baseline" spacing={1}>
-        <Typography variant="subtitle2">Selected Month: </Typography>
+        <Typography variant="subtitle2">Monthly Total: </Typography>
         <Typography variant="subtitle2" color="text.secondary">
-          {fNumber(purchase?.month)}
+          {fCurrency(purchase?.monthlyTotal)}
         </Typography>
       </Stack>
-      <Stack direction="row" justifyContent="space-between" alignItems="baseline" spacing={1}>
-        <Typography variant="subtitle2">Total Price:</Typography>
-        <Typography variant="subtitle2" color="text.secondary">
-          {fCurrency(purchase?.total)}
-        </Typography>
-      </Stack>
-      <Stack direction="row" justifyContent="space-between" alignItems="baseline" spacing={1}>
-        <Typography variant="subtitle2">Price Per Month: </Typography>
-        <Typography variant="subtitle2" color="text.secondary">
-          {fCurrency(
-            purchase?.discount && purchase?.price
-              ? purchase.price - purchase.discount
-              : purchase?.price
-          )}
-        </Typography>
-      </Stack>
+      <Divider />
       <Stack direction="row" justifyContent="space-between" alignItems="baseline" spacing={1}>
         <Typography variant="subtitle2">Amount Due: </Typography>
         <Typography variant="subtitle2" color="text.secondary">
